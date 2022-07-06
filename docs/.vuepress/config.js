3cff10fb8d3e98f46b50216faaba8f511799937c
Jonas Lannoo <jonas.lannoo@vives.be>
2022-07-06 15:57:12 +0200

    added package to add pdfs to markdown

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,9 @@
 module.exports = {
-    title: 'Hello VuePress',
-    description: 'Just playing around'
+    title: 'AI@EDGE',
+    description: 'Website for the VLAIO TETRA AI@EDGE project',
+    markdown: {
+        config: md => {
+          md.use(require('markdown-it-pdf'))
+        }
+      }
   }
