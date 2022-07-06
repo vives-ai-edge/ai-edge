24ff3cb9ca067e0ea3a83eece19cee3f89bcd53d
Jonas Lannoo <jonas.lannoo@vives.be>
2022-07-06 16:51:39 +0200

    added link to iot-incubator and eavise website

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,5 +5,11 @@ module.exports = {
         config: md => {
           md.use(require('markdown-it-pdf'))
         }
-      }
+      },
+    themeConfig: {
+    nav: [
+        { text: 'IoT Incubator', link: 'https://iot-incubator.be/' },
+        { text: 'EAVISE', link: 'https://iiw.kuleuven.be/onderzoek/eavise' }
+        ]
+    }
   }
