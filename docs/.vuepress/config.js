297c2a03b92c5b77f2eca02267cfe07fd633a55d
Jonas Lannoo <jonas.lannoo@vives.be>
2022-07-06 17:17:51 +0200

    updated showurl config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,9 @@ module.exports = {
     description: 'Website for the VLAIO TETRA AI@EDGE project',
     markdown: {
         config: md => {
-          md.use(require('markdown-it-pdf'));
+          md.use(require(markdown-it-pdf), {
+            showUrl: true
+          });
           const pdfUrl = "https://ai-edge.be/AI-EDGE-Slotsymposium.pdf";
           md.render(`@[pdf](${pdfUrl})`);
         }
