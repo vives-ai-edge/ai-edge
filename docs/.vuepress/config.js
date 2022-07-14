09f8a438fef96323e629c75b26f3cc12f4494f34
Jonas Lannoo <jonas.lannoo@vives.be>
2022-07-14 13:59:32 +0200

    added sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    title: 'AI@EDGE - Homepage',
+    title: 'AI@EDGE',
     description: 'Website for the VLAIO TETRA AI@EDGE project',
     markdown: {
         config: md => {
@@ -13,6 +13,20 @@ module.exports = {
         nav: [
             { text: 'IoT Incubator', link: 'https://iot-incubator.be/' },
             { text: 'EAVISE', link: 'https://iiw.kuleuven.be/onderzoek/eavise' }
+        ],
+        sidebar: [
+          '/',
+          {
+            title: 'Usergroup Meetings',
+            collapsable: false,
+            children: [
+              ['/meetings/usergroup1', 'UG Meeting 1'],
+              ['/meetings/usergroup2', 'UG Meeting 2'],
+              ['/meetings/usergroup3', 'UG Meeting 3'],
+              ['/meetings/usergroup4', 'UG Meeting 4'],
+              ['/meetings/finalsymposium', 'Final Symposium']
+            ]
+          },
         ]
       },
     serviceWorker: true
